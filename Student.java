e7a409f3375f15f7586a144d94bbeef02fbb4e7f
Arvind Ravi <ar2au@virginia.edu>
2018-06-02 19:06:40 -0400

    Create enroll and tuition methods

--- Student.java
+++ Student.java
@@ -3,8 +3,8 @@ public class Student {
     private String lastName;
     private int gradeYear;
     private String studentID;
-    private String courses;
-    private String tuitionBalance;
+    private String courses = "";
+    private int tuitionBalance = 0;
     private static int costOfCourse = 600;
     private static int id = 1000;
 
@@ -36,10 +36,41 @@ public class Student {
     }
 
     // Enroll in courses
+    public void enroll() {
+        // Get inside a loop, user hits 0
+        while(true) {
+            System.out.println("Enter course to enroll (Q to quit): ");
+            // Scanner in = new Scanner(System.in);
+            // String course = in.nextLine();
+            String course = "Q";
+            if (!course.equals("Q")) {
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            } else {
+                break;
+            }
+        } 
+
+        System.out.println("ENROLLED IN: " + courses);
+        System.out.println("TUITION BALANCE: " + tuitionBalance);
+    }
 
     // View balance
+    public void viewBalance() {
+        System.out.println("Your balance is: $" + tuitionBalance);
+    }
 
     // Pay tuition
+    public void payTuition() {
+        viewBalance();
+        // System.out.print("Enter your payment: $");
+        // Scanner in = new Scanner(System.in);
+        // int payment = in.nextInt();
+        int payment = 500;
+        tuitionBalance = tuitionBalance - payment;
+        System.out.println("Thank you for your payment of: $" + payment);
+        viewBalance();
+    }
 
     // Show status
 }
--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -1,7 +1,11 @@
 public class StudentDatabaseApp {
     public static void main(String[] args) {
         Student stu1 = new Student();
+        stu1.enroll();
+        stu1.payTuition();
+
         // Ask how many users we want to add
+
         // Create a number of new students
     }
 }
