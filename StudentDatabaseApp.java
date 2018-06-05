77d43708c4e59313298797cdc9510660d7dd13b7
Arvind Ravi <ar2au@virginia.edu>
2018-06-04 20:32:38 -0400

    Create array of Student objects

--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -1,22 +1,21 @@
 public class StudentDatabaseApp {
     public static void main(String[] args) {
-        Student stu1 = new Student();
-        stu1.enroll();
-        stu1.payTuition();
-        System.out.println(stu1.toString());
-
-        Student stu2 = new Student();
-        stu2.enroll();
-        stu2.payTuition();
-        System.out.println(stu2.toString());
-
-        Student stu3 = new Student();
-        stu3.enroll();
-        stu3.payTuition();
-        System.out.println(stu3.toString());
-
         // Ask how many users we want to add
+        // System.out.print("Enter number of new students to enroll: ");
+        // Scanner in = new Scanner(System.in);
+        // int numOfStudents = in.nextInt();
+        int numOfStudents = 2;
+        Student[] students = new Student[numOfStudents];
 
-        // Create a number of new students
+        // Create n number of new students
+        for (int n = 0; n < numOfStudents; n++) {
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+        }
+
+        for (int n = 0; n < numOfStudents; n++) {
+            System.out.println(students[n].toString());
+        }
     }
 }
